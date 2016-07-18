2b9c369543d9a53ac92251bdd1fd3f88e27b971b
bichvt <bichvt233@gmail.com>
2016-07-18 09:11:47 +0700

    add function get data from excel file and use assert to verify

--- src/Test_NG/RegisterScript.java
+++ src/Test_NG/RegisterScript.java
@@ -1,13 +1,17 @@
 package Test_NG;
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import Study_Selenium.Common;
-import Study_Selenium.Logout;
-import Study_Selenium.Register;
+import Test_NG.Common;
+import Test_NG.Logout;
+import Test_NG.Register;
 
 /**
 2. Register account
@@ -23,10 +27,13 @@ URL: http://live.guru99.com/index.php/customer/account/create/
  */
 
 public class RegisterScript {
-	static WebDriver driver = new FirefoxDriver();
+	static WebDriver driver;
 	static Common cm;
 	static String registPage = "http://live.guru99.com/index.php/customer/account/create/";
 	static Register registObj;
+	static String excelFilePath = "D:\\Selenium_Webdriver\\Data Test\\TestData.xlsx";
+	static String sheetName = "Register";
+	static String firstName, lastName, emailAddress, pwd, pwdConf;
 	
 	/**
 	 * 
@@ -36,21 +43,17 @@ public class RegisterScript {
 	 * 
 	 */
 	@Test (priority=1)
-	public static void registerFailNoFirstName() throws InterruptedException{
-		registObj.register(null,"Sen","senvu3@gmail.com","1234567890","1234567890");
+	public static void registerFailNoFirstName() throws InterruptedException,InvalidFormatException, IOException{
+		firstName = cm.getDataFromExcel(excelFilePath, sheetName, 1, 0);
+		lastName = cm.getDataFromExcel(excelFilePath, sheetName, 1, 1);
+		emailAddress = cm.getDataFromExcel(excelFilePath, sheetName, 1, 2);
+		pwd = cm.getDataFromExcel(excelFilePath, sheetName, 1, 3);
+		pwdConf = cm.getDataFromExcel(excelFilePath, sheetName, 1, 4);
+		registObj.register(firstName,lastName,emailAddress,pwd,pwdConf);
 		String expectRegist1 = "This is a required field";
-		String actualRegist1;
-		actualRegist1 = driver.findElement(registObj.ErrorMessNoFirstName).getText();
-		if(actualRegist1.contains(expectRegist1))
-			System.out.println("1. Can not register account without enter First Name: Pass");
-		else
-			System.out.println("1. Can not register account without enter First Name: Fail");
-		driver.findElement(registObj.firstNameElenment).clear(); 
-		driver.findElement(registObj.lastNameElenment).clear(); 
-		driver.findElement(registObj.emailElenment).clear(); 
-		driver.findElement(registObj.passElenment).clear(); 
-		driver.findElement(registObj.confPassElenment).clear(); 
-		Thread.sleep(5000);
+		boolean actualRegist1;
+		actualRegist1 = driver.findElement(registObj.ErrorMessNoFirstName).getText().contains(expectRegist1);;
+		Assert.assertTrue(actualRegist1,"1. Can not register account without enter First Name: Pass");
 	}	
 	/**
 	 * 
@@ -60,21 +63,17 @@ public class RegisterScript {
 	 * 
 	 */
 	@Test (priority=2)
-	public static void registerFailNoLastName() throws InterruptedException{
-		registObj.register("Vu Thi",null,"senvu3@gmail.com","1234567890","1234567890");
+	public static void registerFailNoLastName() throws InterruptedException,InvalidFormatException, IOException{
+		firstName = cm.getDataFromExcel(excelFilePath, sheetName, 2, 0);
+		lastName = cm.getDataFromExcel(excelFilePath, sheetName, 2, 1);
+		emailAddress = cm.getDataFromExcel(excelFilePath, sheetName, 2, 2);
+		pwd = cm.getDataFromExcel(excelFilePath, sheetName, 2, 3);
+		pwdConf = cm.getDataFromExcel(excelFilePath, sheetName, 2, 4);
+		registObj.register(firstName,lastName,emailAddress,pwd,pwdConf);
 		String expectRegist2 = "This is a required field";
-		String actualRegist2;
-		actualRegist2 = driver.findElement(registObj.ErrorMessNoLastName).getText();
-		if(actualRegist2.contains(expectRegist2))
-			System.out.println("2. Can not register account without enter Last Name: Pass");
-		else
-			System.out.println("2. Can not register account without enter Last Name: Fail");
-		driver.findElement(registObj.firstNameElenment).clear(); 
-		driver.findElement(registObj.lastNameElenment).clear(); 
-		driver.findElement(registObj.emailElenment).clear(); 
-		driver.findElement(registObj.passElenment).clear(); 
-		driver.findElement(registObj.confPassElenment).clear(); 
-		Thread.sleep(5000);
+		boolean actualRegist2;
+		actualRegist2 = driver.findElement(registObj.ErrorMessNoLastName).getText().contains(expectRegist2);
+		Assert.assertTrue(actualRegist2,"2. Can not register account without enter Last Name: Pass");
 	}
 	/**
 	 * 
@@ -84,21 +83,17 @@ public class RegisterScript {
 	 * 
 	 */
 	@Test (priority=3)
-	public static void registerFailNoEmail() throws InterruptedException{
-		registObj.register("Vu Thi","Sen",null,"1234567890","1234567890");
+	public static void registerFailNoEmail() throws InterruptedException,InvalidFormatException, IOException{
+		firstName = cm.getDataFromExcel(excelFilePath, sheetName, 3, 0);
+		lastName = cm.getDataFromExcel(excelFilePath, sheetName, 3,1 );
+		emailAddress = cm.getDataFromExcel(excelFilePath, sheetName, 3, 2);
+		pwd = cm.getDataFromExcel(excelFilePath, sheetName, 3, 3);
+		pwdConf = cm.getDataFromExcel(excelFilePath, sheetName, 3, 4);
+		registObj.register(firstName,lastName,emailAddress,pwd,pwdConf);
 		String expectRegist3 = "This is a required field";
-		String actualRegist3;
-		actualRegist3 = driver.findElement(registObj.ErrorMessNoEmail).getText();
-		if(actualRegist3.contains(expectRegist3))
-			System.out.println("3. Can not register account without enter email: Pass");
-		else
-			System.out.println("3. Can not register account without enter email: Fail");
-		driver.findElement(registObj.firstNameElenment).clear(); 
-		driver.findElement(registObj.lastNameElenment).clear(); 
-		driver.findElement(registObj.emailElenment).clear(); 
-		driver.findElement(registObj.passElenment).clear(); 
-		driver.findElement(registObj.confPassElenment).clear(); 
-		Thread.sleep(5000);
+		boolean actualRegist3;
+		actualRegist3 = driver.findElement(registObj.ErrorMessNoEmail).getText().contains(expectRegist3);
+		Assert.assertTrue(actualRegist3,"3. Can not register account without enter email: Pass");
 	}
 	/**
 	 * 
@@ -108,21 +103,17 @@ public class RegisterScript {
 	 * 
 	 */
 	@Test (priority=4)
-	public static void registerFailNoPass() throws InterruptedException{
-		registObj.register("Vu Thi","Sen","vusen3@gmail.com",null,"1234567890");
+	public static void registerFailNoPass() throws InterruptedException,InvalidFormatException, IOException{
+		firstName = cm.getDataFromExcel(excelFilePath, sheetName, 4, 0);
+		lastName = cm.getDataFromExcel(excelFilePath, sheetName, 4, 1);
+		emailAddress = cm.getDataFromExcel(excelFilePath, sheetName, 4, 2);
+		pwd = cm.getDataFromExcel(excelFilePath, sheetName, 4, 3);
+		pwdConf = cm.getDataFromExcel(excelFilePath, sheetName, 4, 4);
+		registObj.register(firstName,lastName,emailAddress,pwd,pwdConf);
 		String expectRegist4 = "This is a required field";
-		String actualRegist4;
-		actualRegist4 = driver.findElement(registObj.ErrorMessNoPass).getText();
-		if(actualRegist4.contains(expectRegist4))
-			System.out.println("4. Can not register account without enter password: Pass");
-		else
-			System.out.println("4. Can not register account without enter password: Fail");
-		driver.findElement(registObj.firstNameElenment).clear(); 
-		driver.findElement(registObj.lastNameElenment).clear(); 
-		driver.findElement(registObj.emailElenment).clear(); 
-		driver.findElement(registObj.passElenment).clear(); 
-		driver.findElement(registObj.confPassElenment).clear(); 
-		Thread.sleep(5000);
+		boolean actualRegist4;
+		actualRegist4 = driver.findElement(registObj.ErrorMessNoPass).getText().contains(expectRegist4);
+		Assert.assertTrue(actualRegist4,"4. Can not register account without enter password: Pass");
 	}
 	/**
 	 * 
@@ -132,21 +123,17 @@ public class RegisterScript {
 	 * 
 	 */
 	@Test (priority=5)
-	public static void registerFailNoConfPass() throws InterruptedException{
-		registObj.register("Vu Thi","Sen","vusen3@gmail.com","1234567890",null);
+	public static void registerFailNoConfPass() throws InterruptedException,InvalidFormatException, IOException{
+		firstName = cm.getDataFromExcel(excelFilePath, sheetName, 5, 0);
+		lastName = cm.getDataFromExcel(excelFilePath, sheetName, 5, 1);
+		emailAddress = cm.getDataFromExcel(excelFilePath, sheetName, 5, 2);
+		pwd = cm.getDataFromExcel(excelFilePath, sheetName, 5, 3);
+		pwdConf = cm.getDataFromExcel(excelFilePath, sheetName, 5, 4);
+		registObj.register(firstName,lastName,emailAddress,pwd,pwdConf);
 		String expectRegist5 = "This is a required field";
-		String actualRegist5;
-		actualRegist5 = driver.findElement(registObj.ErrorMessNoConfPass).getText();
-		if(actualRegist5.contains(expectRegist5))
-			System.out.println("5. Can not register account without enter confirm password: Pass");
-		else
-			System.out.println("5. Can not register account without enter conffirm password: Fail");
-		driver.findElement(registObj.firstNameElenment).clear(); 
-		driver.findElement(registObj.lastNameElenment).clear(); 
-		driver.findElement(registObj.emailElenment).clear(); 
-		driver.findElement(registObj.passElenment).clear(); 
-		driver.findElement(registObj.confPassElenment).clear(); 
-		Thread.sleep(5000);
+		boolean actualRegist5;
+		actualRegist5 = driver.findElement(registObj.ErrorMessNoConfPass).getText().contains(expectRegist5);
+		Assert.assertTrue(actualRegist5,"5. Can not register account without enter confirm password: Pass");		
 	}
 	/**
 	 * 
@@ -156,21 +143,17 @@ public class RegisterScript {
 	 * 
 	 */
 	@Test (priority=6)
-	public static void registerFailConfPassNotMatch() throws InterruptedException{
-		registObj.register("Vu Thi","Sen","vusen3@gmail.com","1234567890","123456789");
-		String expectRegist6 = "Please make sure your passwords match";
+	public static void registerFailConfPassNotMatch() throws InterruptedException,InvalidFormatException, IOException{
+		firstName = cm.getDataFromExcel(excelFilePath, sheetName, 6, 0);
+		lastName = cm.getDataFromExcel(excelFilePath, sheetName, 6, 1);
+		emailAddress = cm.getDataFromExcel(excelFilePath, sheetName, 6, 2);
+		pwd = cm.getDataFromExcel(excelFilePath, sheetName, 6, 3);
+		pwdConf = cm.getDataFromExcel(excelFilePath, sheetName, 6, 4);
+		registObj.register(firstName,lastName,emailAddress,pwd,pwdConf);
+		String expectRegist6 = "Please make sure your passwords match.";
 		String actualRegist6;
 		actualRegist6 = driver.findElement(registObj.ErrorMessNoMatchConfPass).getText();
-		if(actualRegist6.contains(expectRegist6))
-			System.out.println("6. Can not register account because Confirm Password not match Password: Pass");
-		else
-			System.out.println("6. Can not register account because Confirm Password not match Password: Fail");
-		driver.findElement(registObj.firstNameElenment).clear(); 
-		driver.findElement(registObj.lastNameElenment).clear(); 
-		driver.findElement(registObj.emailElenment).clear(); 
-		driver.findElement(registObj.passElenment).clear(); 
-		driver.findElement(registObj.confPassElenment).clear(); 
-		Thread.sleep(5000);
+		Assert.assertEquals(actualRegist6, expectRegist6,"6. Can not register account because Confirm Password not match Password: Pass");
 	}
 	/**
 	 * 
@@ -180,18 +163,16 @@ public class RegisterScript {
 	 * 
 	 */
 	@Test (priority=7)
-	public static void registerFailDuplicateEmail() throws InterruptedException{
-		registObj.register("Vu Thi","Sen","senvuthi@gmail.com","1234567890","1234567890");
-		if(driver.getPageSource().contains("There is already an account with this email address"))
-			System.out.println("7. Can not register account  with duplicate email: Pass");
-		else
-			System.out.println("7. Can not register account  with duplicate email: Fail");
-		driver.findElement(registObj.firstNameElenment).clear(); 
-		driver.findElement(registObj.lastNameElenment).clear(); 
-		driver.findElement(registObj.emailElenment).clear(); 
-		driver.findElement(registObj.passElenment).clear(); 
-		driver.findElement(registObj.confPassElenment).clear(); 
-		Thread.sleep(5000);
+	public static void registerFailDuplicateEmail() throws InterruptedException,InvalidFormatException, IOException{
+		firstName = cm.getDataFromExcel(excelFilePath, sheetName, 7, 0);
+		lastName = cm.getDataFromExcel(excelFilePath, sheetName, 7, 1);
+		emailAddress = cm.getDataFromExcel(excelFilePath, sheetName, 7, 2);
+		pwd = cm.getDataFromExcel(excelFilePath, sheetName, 7, 3);
+		pwdConf = cm.getDataFromExcel(excelFilePath, sheetName, 7, 4);
+		registObj.register(firstName,lastName,emailAddress,pwd,pwdConf);
+		boolean result;
+		result = driver.getPageSource().contains("There is already an account with this email address");
+		Assert.assertTrue(result,"7. Can not register account  with duplicate email: Pass");
 	}
 	/**
 	 * 
@@ -201,25 +182,30 @@ public class RegisterScript {
 	 * 
 	 */
 	@Test (priority=8)
-	public static void registerSuccess() throws InterruptedException{
-		registObj.register("Vu Thi","Sen","bichvuthi1@gmail.com","1234567890","1234567890");
-		if(driver.getPageSource().contains("Thank you for registering with Main Website Store"))
-			System.out.println("8. Register account successfully: Pass");
-		else
-			System.out.println("8. Register account successfully: Fail");
+	public static void registerSuccess() throws InterruptedException,InvalidFormatException, IOException{
+		firstName = cm.getDataFromExcel(excelFilePath, sheetName, 8, 0);
+		lastName = cm.getDataFromExcel(excelFilePath, sheetName, 8, 1);
+		emailAddress = cm.getDataFromExcel(excelFilePath, sheetName, 8, 2);
+		pwd = cm.getDataFromExcel(excelFilePath, sheetName, 8, 3);
+		pwdConf = cm.getDataFromExcel(excelFilePath, sheetName, 8, 4);
+		registObj.register(firstName,lastName,emailAddress,pwd,pwdConf);
+		boolean result;
+		result = driver.getPageSource().contains("Thank you for registering with Main Website Store.");
+		Assert.assertTrue(result, "8. Register account successfully: Pass");	
 		//Logout from the system
 		Logout logoutObj = new Logout(driver);
 		logoutObj.logout();
 	}
 	
-	@BeforeTest
+	@BeforeMethod
 	public static void login()throws InterruptedException {
+		driver = new FirefoxDriver();
 		cm = new Common(driver);
 		cm.openUrl(registPage);
 		registObj = new Register(driver);
 	}
 
-	@AfterTest
+	@AfterMethod
 	public static void closePage(){
 		cm.closeUrl();
 	}
